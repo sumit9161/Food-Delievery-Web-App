9e4e98c88339c6a0e22b22b1dcc026686876324f
Sumit <sumit9161424866@gmail.com>
2023-12-19 12:25:27 +0530

    ongoing

---
+++ restaurant_web_app-master/src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Chapel Hill Shiraz',
+    price: '$56',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Catena Malbee',
+    price: '$59',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'La Vieillw Rose',
+    price: '$44',
+    tags: 'FR | 750 ml',
+  },
+  {
+    title: 'Rhino Pale Ale',
+    price: '$31',
+    tags: 'CA | 750 ml',
+  },
+  {
+    title: 'Irish Guinness',
+    price: '$26',
+    tags: 'IE | 750 ml',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Classic Mojito',
+    price: '$5',
+    tags: 'Rum infused with fresh lime | soda | 30 ml',
+  },
+  {
+    title: 'Retro Margarita',
+    price: '$5',
+    tags: 'Taquila | lime juice | Slice of lime | Rum',
+  },
+  {
+    title: 'Daiquiri',
+    price: '$10',
+    tags: 'Rum | Citrus juice | Sugar',
+  },
+  {
+    title: 'Old Fashioned',
+    price: '$31',
+    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+  },
+  {
+    title: 'Negroni',
+    price: '$26',
+    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Master Chef Kanpur',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Rising Star',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'AA Hospitality',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Outstanding Chef',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+  },
+];
+
+export default { wines, cocktails, awards };
